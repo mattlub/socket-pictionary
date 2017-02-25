2b3ae9b99a015180262dbbaf42a575acc20a8895
Matt Lubel <matt_lubel@hotmail.com>
2017-02-25 22:45:27 +0200

    added name entry functionality

--- public/app.js
+++ public/app.js
@@ -1,2 +1,48 @@
 // initiate socket
 var socket = io();
+
+var app = document.getElementsByClassName('app')[0];
+
+// helper function
+function quickCreateElement(type, opts) {
+  let el = document.createElement(type);
+  for (let attr in opts) {
+    if (attr === 'class') {
+      el.classList.add(opts[attr]);
+    }
+    else if (attr === 'textContent') {
+      el.textContent = opts[attr];
+    }
+    else {
+      el.setAttribute(attr, opts[attr]);
+    }
+  }
+  return el
+}
+
+// Name input logic
+var name;
+var nameInput = quickCreateElement('input', {
+  class: 'name-input',
+  type: 'text',
+  placeholder: 'Your name'
+});
+
+var nameInputListener = function (e) {
+  // if enter pressed and input not empty
+  if (e.keyCode === 13 && this.value.length !== 0) {
+    name = this.value;
+    this.removeEventListener('keypress', nameInputListener);
+    this.classList.add('hidden');
+    data = {name: name, socket: socket};
+    console.log(socket.id);
+    loadGame();
+  }
+}
+
+nameInput.addEventListener('keypress',  nameInputListener);
+app.appendChild(nameInput);
+
+function loadGame () {
+  return
+}
