bcc5848f55c6df22111415ceccd0e59ca3e258c6
Matt Lubel <matt_lubel@hotmail.com>
2017-03-21 01:33:00 +0200

    abstract out render function from app/js

--- public/app.js
+++ public/app.js
@@ -3,32 +3,8 @@ var socket = io();
 
 var app = document.getElementsByClassName('app')[0];
 
-function renderNameInputScreen (app, successCallback) {
-  // Name input logic
-  var name;
-  var nameInput = helpers.quickCreateElement('input', {
-    class: 'name-input',
-    type: 'text',
-    placeholder: 'Your name'
-  });
-
-  var nameInputListener = function (e) {
-    // if enter pressed and input not empty
-    if (e.keyCode === 13 && this.value.length !== 0) {
-      name = this.value;
-      this.removeEventListener('keypress', nameInputListener);
-      this.classList.add('hidden');
-      data = {name: name, socket: socket};
-      console.log(socket.id);
-      successCallback();
-    }
-  }
-  nameInput.addEventListener('keypress',  nameInputListener);
-  app.appendChild(nameInput);
-}
-
-renderNameInputScreen(app, loadGame);
+Render.renderNameInputScreen(app, socket, loadGame);
 
 function loadGame () {
-  return
+  console.log('loading game')
 }
