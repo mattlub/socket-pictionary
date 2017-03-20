edd56f3eeda8f4ce8307d4fec819b17a2d9607f3
Matt Lubel <matt_lubel@hotmail.com>
2017-03-21 01:37:24 +0200

    add state to client side

--- public/app.js
+++ public/app.js
@@ -1,10 +1,25 @@
-// initiate socket
-var socket = io();
+(function () {
 
-var app = document.getElementsByClassName('app')[0];
+  var app = document.getElementsByClassName('app')[0];
 
-Render.renderNameInputScreen(app, socket, loadGame);
+  // initiate socket
+  var socket = io();
 
-function loadGame () {
-  console.log('loading game')
-}
+  // how to hide this?
+  // google hide client side javascript state variable
+  var state = {
+    artist: null,
+    word: null,
+    time: null
+  }
+
+  // to be implemented
+  //state = loadState();
+
+  Render.renderNameInputScreen(app, socket, loadGame);
+
+  function loadGame () {
+    console.log('loading game')
+  }
+
+})();
