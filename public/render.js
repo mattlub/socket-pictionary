74426e9e5a5b1b4eaa42848e687eca81733a27ce
Matt Lubel <matt_lubel@hotmail.com>
2017-04-02 08:18:46 +0300

    move render chat to render file

--- public/render.js
+++ public/render.js
@@ -27,8 +27,33 @@ var Render = (function () {
     app.appendChild(nameInput);
   }
 
+  function renderChat (app, socket, state) {
+    chatTitle = helpers.quickCreateElement('h4', {textContent: 'Live chat:'});
+    chatList = helpers.quickCreateElement('ul', {class: 'chat-list'});
+    chatInput = helpers.quickCreateElement('input');
+    chatInput.addEventListener('keypress', function(e) {
+      if (e.keyCode === 13 && this.value.length > 0) {
+        socket.emit('message', {
+          id: socket.id,
+          message: this.value.slice(0,40)
+        });
+        this.value = '';
+      }
+    });
+    app.appendChild(chatTitle);
+    app.appendChild(chatList);
+    app.appendChild(chatInput);
+
+    socket.addEventListener('message', (info) => {
+      chatList.appendChild(helpers.quickCreateElement('li', {
+        textContent: `${info.name}: ${info.message}`
+      }));
+    })
+  }
+
   return {
-    renderNameInputScreen: renderNameInputScreen
+    renderNameInputScreen: renderNameInputScreen,
+    renderChat: renderChat
   }
 
 })();
