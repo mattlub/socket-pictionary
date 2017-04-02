b337bbb4091c49682e442e0e6aa9b9644d5905c9
Matt Lubel <matt_lubel@hotmail.com>
2017-04-02 08:19:05 +0300

    move renderChat function out of app.js

--- public/app.js
+++ public/app.js
@@ -18,38 +18,15 @@
   });
 
   function startGame () {
+    // don't really want to do this.
     helpers.getState(renderGame);
   }
 
   function renderGame (state) {
     console.log(state);
-    renderChat(appContainer, socket, state);
+    Render.renderChat(appContainer, socket, state);
     // render canvas on socket 'game' event or similar?
-
-  }
-
-  function renderChat (app, socket, state) {
-    chatTitle = helpers.quickCreateElement('h4', {textContent: 'Live chat:'});
-    chatList = helpers.quickCreateElement('ul', {class: 'chat-list'});
-    chatInput = helpers.quickCreateElement('input');
-    chatInput.addEventListener('keypress', function(e) {
-      if (e.keyCode === 13 && this.value.length > 0) {
-        socket.emit('message', {
-          id: socket.id,
-          message: this.value.slice(0,40)
-        });
-        this.value = '';
-      }
-    });
-    app.appendChild(chatTitle);
-    app.appendChild(chatList);
-    app.appendChild(chatInput);
-
-    socket.addEventListener('message', (info) => {
-      chatList.appendChild(helpers.quickCreateElement('li', {
-        textContent: `${info.name}: ${info.message}`
-      }));
-    })
+    Render.renderCanvas(appContainer, socket, state);
   }
 
   Render.renderNameInputScreen(appContainer, socket, startGame);
